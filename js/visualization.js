0fb8294dc029cb04f62a6ca521b28d96cba9ae37
Fabio Mariani <33620930+FabDot-IO@users.noreply.github.com>
2019-01-15 23:06:47 +0100

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -166,12 +166,15 @@ $(document).ready(function(){
 													   "data":[300,50,100],
 													   "backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]}]},
 										     "options":  {
-											    plugins: {
-											     	labels: 
+											    plugins: {[
 												  {
-												    render: 'label'
+												    render: 'label',
 												    //position: 'outside'
+												  },
+												  {
+												    render: 'value'
 												  }
+												]
 												
 											    }}
 										     });
