2b0101e6f63e4c40ae160d466407c942a0fd4a83
Fabio Mariani <33620930+FabDot-IO@users.noreply.github.com>
2019-01-15 16:09:08 +0100

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -151,6 +151,77 @@ $(document).ready(function(){
 								})
 							    }
 							}).error(function() {});
+							    //inizio
+							$.ajax({
+							    dataType: "json",
+							    url: "geojson_folder/testsegna.json",
+							    success: function(datisegn) {
+								datisegn.forEach(function(arrayItem) {
+								    if (arrayItem["Zona"] == feature.properties.Nome_zona) {
+									var dativari = arrayItem["Segnalazioni"]
+									var diameter = 600;
+									var color = d3.scaleOrdinal(d3.schemeCategory20);
+									var bubble = d3.pack(dativari)
+									    .size([diameter, diameter])
+									    .padding(1.5);
+									var svg = d3.select("#grafoSegn")
+									    .append("svg")
+									    .attr("width", diameter)
+									    .attr("height", diameter)
+									    .attr("class", "bubble");
+									var nodes = d3.hierarchy(dativari)
+									    .sum(function(d) { return d.Numero_Segnalazioni; });
+									var node = svg.selectAll(".node")
+									    .data(bubble(nodes).descendants())
+									    .enter()
+									    .filter(function(d){
+									    return  !d.children
+									    })
+									    .append("g")
+									    .attr("class", "node")
+									    .attr("transform", function(d) {
+									    return "translate(" + d.x + "," + d.y + ")";
+									    });
+									node.append("title")
+									    .text(function(d) {
+									    return d.Sottocategoria + ": " + d.Numero_Segnalazioni;
+									    });
+									node.append("circle")
+									    .attr("r", function(d) {
+									    return d.r;
+									    })
+									    .style("fill", function(d,i) {
+									    return color(i);
+									    });
+									node.append("text")
+									    .attr("dy", ".2em")
+									    .style("text-anchor", "middle")
+									    .text(function(d) {
+									    return d.data.Sottocategoria.substring(0, d.r / 3);
+									    })
+									    .attr("font-family", "sans-serif")
+									    .attr("font-size", function(d){
+									    return d.r/5;
+									    })
+									    .attr("fill", "white");
+									node.append("text")
+									    .attr("dy", "1.3em")
+									    .style("text-anchor", "middle")
+									    .text(function(d) {
+									    return d.data.Numero_Segnalazioni;
+									    })
+									    .attr("font-family",  "Gill Sans", "Gill Sans MT")
+									    .attr("font-size", function(d){
+									    return d.r/5;
+									    })
+									    .attr("fill", "white");
+									d3.select(self.frameElement)
+									    .style("height", diameter + "px");
+									    }
+									}
+								    )}     
+								});
+							    //fine
 						    });
 						};
 						})
