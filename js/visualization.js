8ac2b0c18db58003e83ca416617c2bde9c3581bd
Fabio Mariani <33620930+FabDot-IO@users.noreply.github.com>
2019-01-15 20:35:08 +0100

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -159,7 +159,12 @@ $(document).ready(function(){
 								datisegn.forEach(function(arrayItem) {
 								    if (arrayItem["Zona"] == feature.properties.Nome_zona) { 
 									    $('#chartContainerSegn').append('<canvas id="CategorieChart"><canvas>');
-									   new Chart(document.getElementById('CategorieChart'),{"type":"doughnut","data":{"labels":["Red","Blue","Yellow"],"datasets":[{"label":"My First Dataset","data":[300,50,100],"backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]}]}});
+									   new Chart(document.getElementById('CategorieChart'),
+										     {"type":"pie",
+										      "data":{"labels":["Microcriminalità","Degrado ambientale","Degrado sociale"],
+											      "datasets":[{"label":"Categorie Segnalazioni",
+													   "data":[300,50,100],
+													   "backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]}]}});
 									    }
 									})
 								    }
