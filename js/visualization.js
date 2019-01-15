7b6698a813c5d39b705a13d841bdc5fd3006248d
Fabio Mariani <33620930+FabDot-IO@users.noreply.github.com>
2019-01-15 18:09:18 +0100

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -160,11 +160,34 @@ $(document).ready(function(){
 									$('.information').html(arrayItem["Zona"]);
 								    if (arrayItem["Zona"] == feature.properties.Nome_zona) {
 									    $('.information').html("urlo");
-									var diameter = 500, //max size of the bubbles
-									    color    = d3.scale.category20b(); //color category
+									var dataset = {
+									    "children": [{"Name":"Olives","Count":4319},
+										{"Name":"Tea","Count":4159},
+										{"Name":"Mashed Potatoes","Count":2583},
+										{"Name":"Boiled Potatoes","Count":2074},
+										{"Name":"Milk","Count":1894},
+										{"Name":"Chicken Salad","Count":1809},
+										{"Name":"Vanilla Ice Cream","Count":1713},
+										{"Name":"Cocoa","Count":1636},
+										{"Name":"Lettuce Salad","Count":1566},
+										{"Name":"Lobster Salad","Count":1511},
+										{"Name":"Chocolate","Count":1489},
+										{"Name":"Apple Pie","Count":1487},
+										{"Name":"Orange Juice","Count":1423},
+										{"Name":"American Cheese","Count":1372},
+										{"Name":"Green Peas","Count":1341},
+										{"Name":"Assorted Cakes","Count":1331},
+										{"Name":"French Fried Potatoes","Count":1328},
+										{"Name":"Potato Salad","Count":1306},
+										{"Name":"Baked Potatoes","Count":1293},
+										{"Name":"Roquefort","Count":1273},
+										{"Name":"Stewed Prunes","Count":1268}]
+									};
 
-									var bubble = d3.layout.pack()
-									    .sort(null)
+									var diameter = 600;
+									var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+									var bubble = d3.pack(dataset)
 									    .size([diameter, diameter])
 									    .padding(1.5);
 
@@ -174,40 +197,60 @@ $(document).ready(function(){
 									    .attr("height", diameter)
 									    .attr("class", "bubble");
 
-									d3.json("geojson_folder/testsegna.json", function(error, data){
+									var nodes = d3.hierarchy(dataset)
+									    .sum(function(d) { return d.Count; });
 
-									    //convert numerical values from strings to numbers
-									    data = data.map(function(d){ d.value = +d["Numero_Segnalazioni"]; return d; });
+									var node = svg.selectAll(".node")
+									    .data(bubble(nodes).descendants())
+									    .enter()
+									    .filter(function(d){
+										return  !d.children
+									    })
+									    .append("g")
+									    .attr("class", "node")
+									    .attr("transform", function(d) {
+										return "translate(" + d.x + "," + d.y + ")";
+									    });
 
-									    //bubbles needs very specific format, convert data to this.
-									    var nodes = bubble.nodes({children:data}).filter(function(d) { return !d.children; });
+									node.append("title")
+									    .text(function(d) {
+										return d.Name + ": " + d.Count;
+									    });
 
-									    //setup the chart
-									    var bubbles = svg.append("g")
-										.attr("transform", "translate(0,0)")
-										.selectAll(".bubble")
-										.data(nodes)
-										.enter();
+									node.append("circle")
+									    .attr("r", function(d) {
+										return d.r;
+									    })
+									    .style("fill", function(d,i) {
+										return color(i);
+									    });
 
-									    //create the bubbles
-									    bubbles.append("circle")
-										.attr("r", function(d){ return d.r; })
-										.attr("cx", function(d){ return d.x; })
-										.attr("cy", function(d){ return d.y; })
-										.style("fill", function(d) { return color(d.value); });
+									node.append("text")
+									    .attr("dy", ".2em")
+									    .style("text-anchor", "middle")
+									    .text(function(d) {
+										return d.data.Name.substring(0, d.r / 3);
+									    })
+									    .attr("font-family", "sans-serif")
+									    .attr("font-size", function(d){
+										return d.r/5;
+									    })
+									    .attr("fill", "white");
 
-									    //format the text for each bubble
-									    bubbles.append("text")
-										.attr("x", function(d){ return d.x; })
-										.attr("y", function(d){ return d.y + 5; })
-										.attr("text-anchor", "middle")
-										.text(function(d){ return d["Categoria"]; })
-										.style({
-										    "fill":"white", 
-										    "font-family":"Helvetica Neue, Helvetica, Arial, san-serif",
-										    "font-size": "12px"
-										});
-									})
+									node.append("text")
+									    .attr("dy", "1.3em")
+									    .style("text-anchor", "middle")
+									    .text(function(d) {
+										return d.data.Count;
+									    })
+									    .attr("font-family",  "Gill Sans", "Gill Sans MT")
+									    .attr("font-size", function(d){
+										return d.r/5;
+									    })
+									    .attr("fill", "white");
+
+									d3.select(self.frameElement)
+									    .style("height", diameter + "px");
 									    }
 									}
 								    )}     
