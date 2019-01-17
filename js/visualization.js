43d45d10b64b6fda784fa008fefa38c70a6cf60c
Fabio Mariani <33620930+FabDot-IO@users.noreply.github.com>
2019-01-17 21:35:01 +0100

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 						    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 						}).addTo(map);
 
+	
 						var limiti = map.getBounds();
 						map.setMaxBounds(limiti);
 					// Settaggio colore feature geojson
@@ -25,9 +26,29 @@ $(document).ready(function(){
 							fillOpacity: 0.5,
 						    };
 						};
-	
+					
 						var geojson = L.geoJson(zone_bologna, {style: stylegeo, onEachFeature: onEachFeature}).addTo(map);
 						
+						var legend = L.control({position: 'bottomright'});
+
+						legend.onAdd = function (map) {
+
+						    var div = L.DomUtil.create('div', 'info legend'),
+							grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+							labels = [];
+
+						    // loop through our density intervals and generate a label with a colored square for each interval
+						    for (var i = 0; i < grades.length; i++) {
+							div.innerHTML +=
+							    '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+							    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+						    }
+
+						    return div;
+						};
+
+						legend.addTo(map);
+	
 						function highlight (layer) {
 						layer.setStyle({
 							weight: 5,
