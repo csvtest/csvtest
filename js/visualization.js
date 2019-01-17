05727a592de10c838e3bde63590badb9b987e7c5
Fabio Mariani <33620930+FabDot-IO@users.noreply.github.com>
2019-01-17 21:38:54 +0100

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -33,18 +33,22 @@ $(document).ready(function(){
 
 						legend.onAdd = function (map) {
 
-						    var div = L.DomUtil.create('div', 'info legend'),
-							grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-							labels = [];
+							var div = L.DomUtil.create('div', 'info legend'),
+								grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+								labels = [],
+								from, to;
 
-						    // loop through our density intervals and generate a label with a colored square for each interval
-						    for (var i = 0; i < grades.length; i++) {
-							div.innerHTML +=
-							    '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-							    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-						    }
+							for (var i = 0; i < grades.length; i++) {
+								from = grades[i];
+								to = grades[i + 1];
 
-						    return div;
+								labels.push(
+									'<i style="background:' + getColor(from + 1) + '"></i> ' +
+									from + (to ? '&ndash;' + to : '+'));
+							}
+
+							div.innerHTML = labels.join('<br>');
+							return div;
 						};
 
 						legend.addTo(map);
