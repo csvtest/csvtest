c1cc3f7d95877f066189d697e3350e14282b258a
Fabio Mariani <33620930+FabDot-IO@users.noreply.github.com>
2019-01-17 17:07:46 +0100

    Update visualization.js

--- js/visualization.js
+++ js/visualization.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 						function stylegeo(feature) {
 						    return {
 							fillColor: getColor(feature.properties.Censimento2017_zona),
-							fillOpacity: 1.5,
+							fillOpacity: 0.5,
 						    };
 						};
 	
